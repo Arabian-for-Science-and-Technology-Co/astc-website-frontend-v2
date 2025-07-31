e546bb6cacb9bc71147f4317509460978d6c1c6e
Mustafa Saied <mustafasaied97@gmail.com>
2025-08-01 01:31:57 +0300

    baseBtn

---
+++ app/components/ui/BaseButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+    :class="[
+      'text-center text-base font-normal not-italic leading-[normal] tracking-[0.2px] text-black',
+      'w-full max-w-[275px] rounded-3xl bg-white py-[23px] transition-colors hover:bg-white/70',
+    ]"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  text: { type: Array, required: true },
+  btnClass: { type: String }
+})
+</script>
