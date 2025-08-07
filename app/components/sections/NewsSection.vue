935339f76591b34c41f47ee2d48a1e54c27bbea0
Mustafa Saied <mustafasaied97@gmail.com>
2025-08-07 15:08:44 +0300

    add news

--- app/components/sections/NewsSection.vue
+++ app/components/sections/NewsSection.vue
@@ -1,52 +1,52 @@
 <template>
   <section id="NewsSection" class="h-[200px] bg-[#465AE6] pb-[36px] pt-[30px] font-zarid">
-    <div
-      class="scrollWrapper app-container flex h-full items-start justify-start gap-[70px] overflow-auto lg:gap-[95px]"
-    >
-      <div
-        class="group relative flex h-full flex-shrink-0 items-start justify-start pe-[100px] last-of-type:pe-0"
-        v-for="(item, index) in newsData"
-      >
-        <div class="not-italic text-white hover:text-[#0ADF0A]">
-          <h4
-            :class="[
-              'main-title mb-[14px] text-[16px] font-[500] uppercase leading-[19.2px] tracking-[0.16px]',
-              'lg:mb-[2px] lg:text-[32px] lg:font-[700] lg:leading-[38.4px] lg:tracking-[0.32px]'
-            ]"
-          >
-            {{ $fd(item.updated_at, 'MMM DD, YYYY') }}
-          </h4>
-          <h4
-            :class="[
-              'text-truncate-multi w-[220px] text-[20px] font-[200] leading-[21px] tracking-[0.32px]',
-              'lg:w-[580px] lg:pe-[10%] lg:text-[32px] lg:font-[300] lg:leading-[30.4px]'
-            ]"
-          >
-            {{ item?.[`title_${locale}`] }}
-          </h4>
-        </div>
-        <BaseImg
-          densities="x1 x2"
-          format="webp"
-          v-if="item.video_url"
-          :src="item.video_url"
-          class="mb-[17px] h-[87px] w-[133.912px] self-end rounded-[10px] object-cover lg:mb-0 lg:h-[102px] lg:w-[157px]"
-          :alt="`video`"
-        />
+    <ScrollWithDragWrapper class=" ">
+      <div class="app-container flex h-full items-start justify-start gap-[70px] lg:gap-[95px]">
         <div
-          class="absolute bottom-0 end-0 flex flex-col items-start justify-start text-[40px] font-[200] not-italic leading-[49.4px] tracking-[0.52px] text-[#0ADF0A] group-last-of-type:hidden lg:text-[52px]"
+          class="group relative flex h-full flex-shrink-0 items-start justify-start pe-[100px] last-of-type:pe-0"
+          v-for="(item, index) in newsData"
         >
-          <span>#</span>
-          <span>#</span>
-          <span>#</span>
+          <div class="not-italic text-white hover:text-[#0ADF0A]">
+            <h4
+              :class="[
+                'main-title mb-[14px] text-[16px] font-[500] uppercase leading-[19.2px] tracking-[0.16px]',
+                'lg:mb-[2px] lg:text-[32px] lg:font-[700] lg:leading-[38.4px] lg:tracking-[0.32px]'
+              ]"
+            >
+              {{ $fd(item.updated_at, 'MMM DD, YYYY') }}
+            </h4>
+            <h4
+              :class="[
+                'text-truncate-multi w-[220px] text-[20px] font-[200] leading-[21px] tracking-[0.32px]',
+                'lg:w-[580px] lg:pe-[10%] lg:text-[32px] lg:font-[300] lg:leading-[30.4px]'
+              ]"
+            >
+              {{ item?.[`title_${locale}`] }}
+            </h4>
+          </div>
+          <BaseImg
+            densities="x1 x2"
+            format="webp"
+            v-if="item.video_url"
+            :src="item.video_url"
+            class="mb-[17px] h-[87px] w-[133.912px] self-end rounded-[10px] object-cover lg:mb-0 lg:h-[102px] lg:w-[157px]"
+            :alt="`video`"
+          />
+          <div
+            class="absolute bottom-0 end-0 flex flex-col items-start justify-start text-[40px] font-[200] not-italic leading-[49.4px] tracking-[0.52px] text-[#0ADF0A] group-last-of-type:hidden lg:text-[52px]"
+          >
+            <span>#</span>
+            <span>#</span>
+            <span>#</span>
+          </div>
         </div>
+        <button
+          class="my-auto whitespace-nowrap text-center align-middle font-durke text-[20px] font-medium uppercase not-italic leading-[120%] tracking-[0.32px] text-[#0ADF0A] hover:text-white lg:text-[32px]"
+        >
+          {{ $t('all_news') }} /
+        </button>
       </div>
-      <button
-        class="my-auto whitespace-nowrap text-center align-middle font-durke text-[20px] font-medium uppercase not-italic leading-[120%] tracking-[0.32px] text-[#0ADF0A] hover:text-white lg:text-[32px]"
-      >
-        {{ $t('all_news') }} /
-      </button>
-    </div>
+    </ScrollWithDragWrapper>
   </section>
 </template>
 
@@ -82,17 +82,6 @@ const data = [
 ]
 </script>
 <style>
-.scrollWrapper {
-  overflow-x: auto;
-  overflow-y: hidden;
-  cursor: pointer;
-  -webkit-user-select: none; /* Safari */
-  -ms-user-select: none; /* IE 10 and IE 11 */
-  user-select: none; /* Standard syntax */
-}
-.scrollWrapper::-webkit-scrollbar {
-  display: none;
-}
 #NewsSection .main-title {
   font-family: 'Druk Wide', sans-serif;
 }
