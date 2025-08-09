e24dca8be61f939dac60dcb2e8290c7076a8e6c8
Mustafa Saied <mustafasaied97@gmail.com>
2025-08-09 03:13:27 +0300

    refactore

--- app/components/layout/app-header/AppHeader.vue
+++ app/components/layout/app-header/AppHeader.vue
@@ -1,109 +1,42 @@
 <template>
-  <section
-    :dir="'ltr'"
-    :class="[
-      'app-container absolute z-40 flex h-fit w-full items-center justify-between bg-transparent transition-colors duration-300',
-      'pt-[--header-pt]'
-    ]"
+  <BaseAppHeader
+    :showLeftLogo="showLeftLogo"
+    :isWhiteLogo="isWhiteLogo"
+    :tabs="tabs"
+    :desktopMenuWidth="desktopMenuRef?.width"
   >
-    <!-- Left Logo Section -->
-    <article
-      @click="navigateTo('/')"
-      v-if="showLeftLogo"
-      class="-mt-[17px] hidden gap-[27px] lg:flex"
-    >
-      <BaseImg
-        densities="x1 x2"
-        format="webp"
-        :src="isWhiteLogo ? settings.logo_light : settings.logo_dark"
-        :class="['mb-[7px] h-[68px] w-[130px] self-start hover:cursor-pointer']"
-        :fallback="ASC_Logo_Main"
-      />
-      <h3
-        v-if="settings?.[`title_${locale}`]"
-        :class="[
-          'max-w-[155px] self-end text-xl font-[400] not-italic leading-[21px] tracking-[0.2px] text-[#18264A]',
-          isWhiteLogo ? 'text-white' : 'text-[#18264A]'
-        ]"
-      >
-        {{ settings?.[`title_${locale}`] }}
-      </h3>
-    </article>
-
-    <!-- Center Vision Logo -->
-    <SaudiVision
-      :isWhiteLogo="isWhiteLogo"
-      :showLeftLogo="showLeftLogo"
-      class="mb-[10px] self-end"
-    />
-
-    <ClientOnly>
-      <!-- Mobile Logo -->
-      <div
-        @click="navigateTo('/')"
-        :class="['-ms-[50px] -mt-6 mb-0 block h-[55px] w-[106px]', 'lg:hidden']"
-      >
-        <BaseImg
-          densities="x1 x2"
-          format="webp"
-          :src="isWhiteLogo ? settings.logo_light : settings.logo_dark"
-          class="h-full w-full"
-          :fallback="ASC_Logo_Main"
-        />
-      </div>
-    </ClientOnly>
-
-    <div class="mobileMenu-placeholder">
-      <MobileMenu
-        :isWhiteLogo="isWhiteLogo"
-        class="gird fixed end-[--container-ps] top-[--header-pt] z-[100] lg:hidden"
+    <template #languageSwitcher>
+      <LanguageSwitcher :tabsClass="tabsClass" :selectedTabClass="selectedTabClass" />
+    </template>
+    <template #desktopMenu>
+      <DesktopMenu
+        :dir="'ltr'"
+        ref="desktopMenuRef"
+        class="fixed end-[--container-pe] top-[--header-pt] z-[60] hidden lg:flex"
         :tabs="tabs"
-      />
-    </div>
-
-    <article class="hidden items-center gap-[40px] lg:flex">
-      <LanguageSwitcher
-        :tabsClass="enableHover && tabsClass"
-        :selectedTabClass="enableHover && selectedTabClass"
-      />
-      <ClientOnly>
-        <div class="desktopMenu-placeholder" :style="{ width: desktopMenuRef?.width + 'px' }">
-          <DesktopMenu
-            :dir="'ltr'"
-            ref="desktopMenuRef"
-            class="fixed end-[--container-pe] top-[--header-pt] z-[60] hidden lg:flex"
-            :tabs="tabs"
-            :enableHover="enableHover"
-            :tabsClass="tabsClass"
-            :selectedTabClass="selectedTabClass"
-          />
-        </div>
-      </ClientOnly>
-    </article>
-  </section>
+        :tabsClass="tabsClass"
+        :selectedTabClass="selectedTabClass"
+    /></template>
+  </BaseAppHeader>
 </template>
 
 <script setup>
-const ASC_Logo_Main = '/Icons/ASC_Logo_Main.svg'
-
 const props = defineProps({
   showLeftLogo: { type: Boolean, default: true },
-  enableHover: { type: Boolean, default: true },
   isWhiteLogo: { type: Boolean, default: false },
   tabsClass: { type: String, default: '' },
   selectedTabClass: { type: String, default: '' }
 })
-
-import LanguageSwitcher from '~/components/LanguageSwitcher.vue'
-const { locale } = useI18n()
-
+const desktopMenuRef = ref(null)
 const { pages } = usePages()
-const { settings } = useWebsiteSettings()
 const route = useRoute()
-// const isWhiteBg = computed(() =>
-//   ['/products-and-solutions', '/categories-details','/contact','/about'].some((p) => route.fullPath.startsWith(p))
-// )
-
+const { locale } = useI18n()
+watch(
+  () => props.isWhiteLogo,
+  (isWhiteLogo) => {
+    console.log('isWhiteLogo', isWhiteLogo)
+  }
+)
 const tabs = computed(() =>
   pages.value
     .filter((page) => page.slug != 'home')
@@ -114,6 +47,4 @@ const tabs = computed(() =>
       isNew: page.slug == 'news' && route.path != '/news'
     }))
 )
-
-const desktopMenuRef = ref(null)
 </script>
---
+++ app/components/layout/app-header/BaseAppHeader.vue
@@ -0,0 +1,88 @@
+<template>
+  <section
+    :dir="'ltr'"
+    :class="[
+      'app-container absolute z-40 flex h-fit w-full items-center justify-between bg-transparent transition-colors duration-300',
+      'pt-[--header-pt]'
+    ]"
+  >
+    <!-- Left Logo Section -->
+    <article
+      @click="navigateTo('/')"
+      v-if="showLeftLogo"
+      class="-mt-[17px] hidden gap-[27px] lg:flex"
+    >
+      <BaseImg
+        densities="x1 x2"
+        format="webp"
+        :src="isWhiteLogo ? settings.logo_light : settings.logo_dark"
+        :class="['mb-[7px] h-[68px] w-[130px] self-start hover:cursor-pointer']"
+        :fallback="ASC_Logo_Main"
+      />
+      <h3
+        v-if="settings?.[`title_${locale}`]"
+        :class="[
+          'max-w-[155px] self-end text-xl font-[400] not-italic leading-[21px] tracking-[0.2px] text-[#18264A]',
+          isWhiteLogo ? 'text-white' : 'text-[#18264A]',
+          'hidden xl:block'
+        ]"
+      >
+        {{ settings?.[`title_${locale}`] }}
+      </h3>
+    </article>
+
+    <!-- Center Vision Logo -->
+    <SaudiVision
+      :isWhiteLogo="isWhiteLogo"
+      :showLeftLogo="showLeftLogo"
+      class="mb-[10px] self-end"
+    />
+
+    <ClientOnly>
+      <!-- Mobile Logo -->
+      <div
+        @click="navigateTo('/')"
+        :class="['-ms-[50px] -mt-6 mb-0 block h-[55px] w-[106px]', 'lg:hidden']"
+      >
+        <BaseImg
+          densities="x1 x2"
+          format="webp"
+          :src="isWhiteLogo ? settings.logo_light : settings.logo_dark"
+          class="h-full w-full"
+          :fallback="ASC_Logo_Main"
+        />
+      </div>
+    </ClientOnly>
+
+    <div class="mobileMenu-placeholder">
+      <MobileMenu
+        :isWhiteLogo="isWhiteLogo"
+        class="gird fixed end-[--container-ps] top-[--header-pt] z-[100] lg:hidden"
+        :tabs="tabs"
+      />
+    </div>
+
+    <article class="hidden items-center gap-[40px] lg:flex">
+      <slot name="languageSwitcher"></slot>
+      <ClientOnly>
+        <div class="desktopMenu-placeholder" :style="{ width: desktopMenuWidth + 'px' }">
+          <slot name="desktopMenu"></slot>
+        </div>
+      </ClientOnly>
+    </article>
+  </section>
+</template>
+
+<script setup>
+const ASC_Logo_Main = '/Icons/ASC_Logo_Main.svg'
+
+const props = defineProps({
+  showLeftLogo: { type: Boolean, default: true },
+  isWhiteLogo: { type: Boolean, default: false },
+  desktopMenuWidth: { type: [String, Number], default: '' },
+  tabs: { type: [Array, undefined] }
+})
+
+const { locale } = useI18n()
+const { settings } = useWebsiteSettings()
+</script>
