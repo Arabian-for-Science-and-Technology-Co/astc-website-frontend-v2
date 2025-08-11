ef39df05073ae0c0fa305e31815d070d1d2c48d0
Mustafa Saied <mustafasaied97@gmail.com>
2025-08-11 22:48:39 +0300

    product-page

---
+++ app/components/product-card/SmallProductCard.vue
@@ -0,0 +1,64 @@
+<template>
+  <figure
+    @click="navigateTo(`/product-details/${data?.id}`)"
+    :class="['imgs-contianer__card', wide && 'imgs-contianer__card--wide']"
+  >
+    <BaseImg
+      densities="x1 x2"
+      format="webp"
+      :key="i"
+      :src="data?.image"
+      :class="['imgs-contianer__card__img']"
+      :alt="`Image ${i + 1} representing solution`"
+      loading="lazy"
+      decoding="async"
+    />
+    <figcaption class="w-full">
+      <h3 v-if="data?.[`title_${locale}`]" class="imgs-contianer__card__title">
+        {{ data?.[`title_${locale}`] }}
+      </h3>
+      <!-- <p
+        v-if="data?.[`meta_desc_${locale}`]"
+        :class="['imgs-contianer__card__desc', wide && 'imgs-contianer__card__desc--wide']"
+      >
+        {{ data?.[`meta_desc_${locale}`] }}
+      </p> -->
+    </figcaption>
+  </figure>
+</template>
+
+<script setup>
+defineProps({
+  data: { type: Object, default: () => ({}) },
+  wide: { type: Boolean, default: false }
+})
+const { locale } = useI18n()
+</script>
+<style scoped>
+.imgs-contianer__card {
+  @apply flex w-[90.3px] flex-col items-center gap-[11.67px];
+  @apply lg:w-[120px] lg:gap-[20px];
+  @apply 3xl:w-[160px];
+}
+.imgs-contianer__card--wide {
+  @apply w-[180px] lg:w-[120px] 3xl:w-[160px];
+}
+.imgs-contianer__card__img {
+  @apply h-[90.3px] w-full overflow-hidden rounded-[20.845px] transition-all hover:translate-y-[-5px];
+  @apply lg:h-[120px] lg:rounded-[27.692px];
+  @apply 3xl:h-[160px] 3xl:rounded-[36.923px];
+
+  &:hover {
+    filter: drop-shadow(0 16px 28px rgba(23, 120, 255, 0.5));
+  }
+}
+.imgs-contianer__card__title {
+  @apply w-full text-center text-[16px] font-[500] uppercase not-italic leading-[normal] tracking-[1.28px];
+  @apply lg:text-xl lg:font-[500] lg:tracking-[1.6px];
+}
+/*------------------* */
+.imgs-contianer__card__desc {
+  @apply mt-[3px] w-full px-0 text-center text-xl font-[400] not-italic leading-[21px] tracking-[0.2px];
+  @apply lg:mt-[17px] lg:px-2;
+}
+</style>
--- app/components/sections/ProductsSection.vue
+++ app/components/sections/ProductsSection.vue
@@ -9,42 +9,13 @@
     <article>
       <h2 class="main-title">{{ products?.[`title_${locale}`] }}</h2>
       <div class="imgs-contianer mt-[29px]">
-        <figure v-for="(item, i) in products?.items || []" class="imgs-contianer__card">
-          <BaseImg
-            densities="x1 x2"
-            format="webp"
-            :key="i"
-            :src="item.image"
-            class="imgs-contianer__card__img"
-            :alt="`Image ${i + 1} representing product`"
-          />
-          <figcaption class="imgs-contianer__card__title">
-            {{ item?.[`title_${locale}`] }}
-          </figcaption>
-        </figure>
+        <SmallProductCard v-for="(item, i) in products?.items || []" :data="item" />
       </div>
     </article>
     <article>
       <h2 class="main-title">{{ solutions?.[`title_${locale}`] }}</h2>
       <div class="imgs-contianer mt-[29px]">
-        <figure
-          v-for="(item, i) in solutions?.items || []"
-          class="imgs-contianer__card imgs-contianer__card--wide"
-        >
-          <BaseImg
-            densities="x1 x2"
-            format="webp"
-            :key="i"
-            :src="item.image"
-            class="imgs-contianer__card__img"
-            :alt="`Image ${i + 1} representing solution`"
-            loading="lazy"
-            decoding="async"
-          />
-          <figcaption class="imgs-contianer__card__title">
-            {{ item?.[`title_${locale}`] }}
-          </figcaption>
-        </figure>
+        <SmallProductCard v-for="(item, i) in solutions?.items || []" :data="item" wide />
       </div>
     </article>
   </section>
@@ -69,26 +40,5 @@ const solutions = computed(() =>
 .imgs-contianer {
   @apply flex flex-wrap justify-start gap-x-[23.09px] gap-y-[38.75px];
   @apply lg:gap-[60px];
-}
-.imgs-contianer__card {
-  @apply flex w-[90.3px] flex-col items-center gap-[11.67px];
-  @apply lg:w-[120px] lg:gap-[20px];
-  @apply 3xl:w-[160px];
-}
-.imgs-contianer__card--wide {
-  @apply w-[180px] lg:w-[120px] 3xl:w-[160px];
-}
-.imgs-contianer__card__img {
-  @apply h-[90.3px] w-full overflow-hidden rounded-[20.845px] transition-all hover:translate-y-[-5px];
-  @apply lg:h-[120px] lg:rounded-[27.692px];
-  @apply 3xl:h-[160px] 3xl:rounded-[36.923px];
-
-  &:hover {
-    filter: drop-shadow(0 16px 28px rgba(23, 120, 255, 0.5));
-  }
-}
-.imgs-contianer__card__title {
-  @apply w-full text-center text-[16px] font-[500] uppercase not-italic leading-[normal] tracking-[1.28px];
-  @apply lg:text-xl lg:font-[500] lg:tracking-[1.6px];
 }
 </style>
