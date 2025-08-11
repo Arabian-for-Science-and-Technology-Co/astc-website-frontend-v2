d563b89a14e7068ed0ddff0f2404dbed5e596565
Mustafa Saied <mustafasaied97@gmail.com>
2025-08-12 00:50:28 +0300

    header

--- app/pages/product-details/[slug].vue
+++ app/pages/product-details/[slug].vue
@@ -1,9 +1,33 @@
 <template>
-  <div
-    :class="[
-      'bg flex min-h-screen flex-col bg-[#0D1667] pb-[209px] pt-[--header-height] font-zarid text-white lg:pb-[300px] 3xl:pb-[280px]'
-    ]"
-  >
+  <div :class="['bg-[#F0F0F0] font-zarid', 'lg:pb-[300px]', '3xl:pb-[280px]']" :styl>
+    <div
+      :class="[
+        'bg flex h-screen max-h-[968px] flex-col justify-end bg-[#0D1667] pb-[60px] pt-[--header-height] text-white',
+        'lg:pb-[117px]',
+        '3xl:pb-[147px]'
+      ]"
+      :style="{
+        backgroundImage: `url(${productData?.cover_image})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundPosition: 'no-repeat'
+      }"
+    >
+      <header class="app-container text-white">
+        <h6
+          :class="[
+            'text-[20px] font-[600] leading-[normal] tracking-[0.2px]',
+            'lg:text-[30px] lg:leading-[33px] lg:tracking-[0.3px]'
+          ]"
+        >
+          {{ productData?.category?.[`title_${locale}`] || 'All products' }}
+        </h6>
+        <h4 :class="['head-title mt-[22px]', 'lg:mt-[15px]']">
+          {{ productData?.[`title_${locale}`] }}
+        </h4>
+      </header>
+    </div>
+    <div class="min-h-screen"></div>
     <BaseModal
       v-model:open="modalOpen"
       containerClass="w-full !mx-0 max-w-[310px] sm:max-w-[500px] 3xl:!max-w-[612px] !bg-transparent"
@@ -25,7 +49,7 @@
 definePageMeta({
   layoutProps: {
     isWhiteLogo: true,
-    selectedTabClass: 'bg-[#0D1667] text-white'
+    selectedTabClass: 'bg-[#010101] text-white'
   }
 })
 const selectedCertificate = ref(null)
@@ -41,10 +65,14 @@ const { data: productData } = await useAsyncData(
 )
 </script>
 <style scoped>
+.head-title {
+  @apply text-[52px] font-[200] leading-[49.4px] tracking-[0.52px];
+  @apply lg:text-[140px] lg:leading-[133px] lg:tracking-[1.4px];
+}
 .bg {
   background:
-    linear-gradient(191deg, rgba(13, 22, 103, 0) 23.08%, #0d1667 70.2%),
-    url('~/assets/images/about/background.jpg');
+    url('~/assets/images/products/raad-2.jpg') lightgray 0px 62.383px / 100% 132.248% no-repeat,
+    #d9d9d9;
 }
 .app-container--edit {
   @media (min-width: 1600px) {
