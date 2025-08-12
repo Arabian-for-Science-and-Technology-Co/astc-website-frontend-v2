928e01546174b36b22127aa581a01a9a20e67ccb
Mustafa Saied <mustafasaied97@gmail.com>
2025-08-13 02:58:36 +0300

    update

--- app/components/global/RequestDocumentModal.vue
+++ app/components/global/RequestDocumentModal.vue
@@ -135,4 +135,5 @@ async function submit() {
     isLoading.value = false
   }
 }
+ 
 </script>
--- app/components/ui/BaseModal.vue
+++ app/components/ui/BaseModal.vue
@@ -142,13 +142,4 @@ onUnmounted(() => {
 })
 </script>
 
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.25s ease;
-}
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
+<style></style>
