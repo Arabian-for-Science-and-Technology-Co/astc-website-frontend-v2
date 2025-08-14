43655dd5d59a764aceb2e2f19dd13780189b6ba4
Mustafa Saied <mustafasaied97@gmail.com>
2025-08-14 19:22:07 +0300

    update restructure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -73,6 +73,9 @@ export default defineNuxtConfig({
     }
   ],
   // ignore: ["pages/**/_*"],
+  pages: {
+    pattern: ['**/*.vue', '!**/_*']
+  },
 
   //----------------------others----------------------
   experimental: {
