a57e557378aaa5d5b82e6c3a13a3be5eae166430
Mustafa Saied <mustafasaied97@gmail.com>
2025-08-15 11:54:47 +0300

    update

--- app/pages/news/[slug].vue
+++ app/pages/news/[slug].vue
@@ -1,35 +1,62 @@
 <template>
-  <div
-    :class="[
-      'app-container-small min-h-screen bg-[#465AE6] pb-[180px] pt-[--header-height] font-zarid text-white',
-      'lg:pb-[163px]',
-      '3xl:pb-[140px]'
-    ]"
-  >
-    <div class="mt-[128px] flex flex-wrap items-start justify-between gap-y-[19px] lg:mt-[129px]">
+  <div :class="['min-h-screen bg-[#F0F0F0] pt-[--header-height] font-zarid text-black']">
+    <section class="app-container-small mt-[128px] min-h-screen lg:mt-[129px]">
       <h1
         :class="[
-          'font-durke text-[48px] font-[500] leading-[120%] tracking-[0.48px]',
-          '3xl:text-[70px] 3xl:font-[500] 3xl:leading-[120%] 3xl:tracking-[0.7px]'
+          'font-durke text-[28px] font-[500] leading-[120%] tracking-[0.28px] text-[#0ADF0A]'
         ]"
       >
         {{ $t('news') }} /
       </h1>
-      <p
-        :class="[
-          'flex items-center justify-start gap-[47px] text-[30px] font-[600] leading-[110%] tracking-[0.3px] text-[#0ADF0A] lg:mt-[12px]',
-          'lg:gap-10'
-        ]"
-      >
-        <span>
-          {{ $fd(newsListData[0]?.updated_at, 'll') }}
-        </span>
-        <span> {{ $fd(newsListData[0]?.updated_at, 'h:mm A') }} </span>
-      </p>
-    </div>
+      <article>
+        <h4
+          :class="[
+            'transition-colors group-hover:text-[#0ADF0A]',
+            'text-[30px] font-[600] leading-[110%] tracking-[0.3px]'
+          ]"
+        >
+          {{ $fd(newsData?.updated_at, 'll') }}
+        </h4>
+        <p
+          :class="[
+            'mt-[8px] text-[30px] font-[400] leading-[110%] transition-colors group-hover:text-[#0ADF0A] lg:tracking-[0.3px]',
+            'lg:mt-[6px] lg:text-[52px] lg:font-[200] lg:leading-[95%] lg:tracking-[0.52px]'
+          ]"
+        >
+          {{ newsData?.[`title_${locale}`] }}
+        </p>
+        <BaseImg
+          densities="x1 x2"
+          format="webp"
+          v-if="newsData?.image"
+          :src="newsData?.image"
+          class="mt-[24px] aspect-[180/103] w-[279.623px] self-end rounded-[10px] object-cover lg:mt-[42px] lg:aspect-[7/4] lg:w-[280px]"
+          :alt="`video`"
+        />
+        <NewsTemplate :content="newsData?.[`content_${locale}`]" />
+        <div>
+          <div>
+            <h3>{{ newsData?.testimonial?.[`content_${locale}`] }}</h3>
+            <BaseImg
+              densities="x1 x2"
+              format="webp"
+              v-if="newsData?.testimonial?.image"
+              :src="newsData?.testimonial?.image"
+              class="mt-[24px] aspect-[180/103] bg-slate-500/50 w-[279.623px] self-end rounded-[10px] object-cover lg:mt-[42px] lg:aspect-[7/4] lg:w-[280px]"
+              :alt="`video`"
+            />
+          </div>
+          <p>
+            {{ newsData?.testimonial?.[`name_${locale}`] }}
+            {{ newsData?.testimonial?.[`position_${locale}`] }}
+          </p>
+        </div>
+      </article>
+    </section>
     <ClientOnly>
       <NewsList
-        :newsListData="newsListData"
+        class="app-container-small bg-[#465AE6] text-white"
+        :listData="newsListData"
         :isLoadingMore="isLoadingMore"
         :totalPages="totalPages"
         v-model:currentPage="currentPage"
@@ -40,10 +67,10 @@
 
 <script setup>
 import NewsList from './_components/NewsList.vue'
+import NewsTemplate from './_components/NewsTemplate.vue'
 definePageMeta({
   layoutProps: {
-    isWhiteLogo: true,
-    tabsClass: null,
+    isWhiteLogo: false,
     selectedTabClass: 'bg-[#465AE6] text-white'
   }
 })
@@ -53,7 +80,7 @@ const { locale } = useI18n()
 const customFetch = useCustomFetch()
 const { data: newsData } = await useAsyncData(
   () => `news-details:${route.params.slug}`,
-  () => customFetch(`/website/news${route.params.slug}`),
+  () => customFetch(`/website/news/${route.params.slug}`),
   {
     transform: (res) => res.data || [],
     watch: [() => route.params.slug]
@@ -77,6 +104,13 @@ const {
   defaultPerPage: 3,
   watch: [() => route.params.slug]
 })
+watch(
+  newsListData,
+  (newsListData) => {
+    console.log('newsListData', newsListData)
+  },
+  { deep: true }
+)
 usePageHead(() => ({
   title: newsData.value?.[`meta_title_${locale.value}`],
   description: newsData.value?.[`meta_description_${locale.value}`],
