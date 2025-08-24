0f2a5053be84c04ecab4078d50b77989077f4325
Mustafa Saied <mustafasaied97@gmail.com>
2025-08-24 14:20:02 +0300

    animated-grids

--- app/components/animated-grids/StaticGride.vue
+++ app/components/animated-grids/StaticGride.vue
@@ -1,36 +1,20 @@
 <template>
   <div class="StaticGride-container">
-    <template v-for="(node, index) in nodes" :key="index">
+    <template v-for="(node, index) in nodes" :key="node.position">
       <div v-if="node.isBig" class="node node-2"></div>
-
-      <template v-if="node.isAnimated">
-        <div
-          class="node node--animated"
-          :class="{
-            'node-2': activeNode === node.position,
-            'node-1': activeNode !== node.position
-          }"
-        ></div>
-      </template>
 
       <div v-if="node.isSmall" class="node node-1"></div>
     </template>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const props = defineProps({
-  delay: {
-    type: Number,
-    default: 0
-  },
   smallDotPosition: {
     type: Number,
     default: 15
   }
 })
-
-const activeNode = ref(props.smallDotPosition[0])
 
 const totalLength = 1000
 const nodes = computed(() =>
@@ -65,11 +49,7 @@ const nodes = computed(() =>
   transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
   transition-duration: 500ms;
 }
-.node--animated {
-  transition-property: all;
-  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-  transition-duration: 500ms;
-}
+
 .node-1 {
   transform: scale(0.5);
   align-self: flex-end;
